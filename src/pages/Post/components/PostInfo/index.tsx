8480e0861643275ad5b12079db350efa75b1761b
henriquesbarros <henriquesbarroshs@gmail.com>
2022-11-02 11:31:25 -0300

    feat: adding API integration in post info

--- src/pages/Post/components/PostInfo/index.tsx
+++ src/pages/Post/components/PostInfo/index.tsx
@@ -7,42 +7,58 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useNavigate } from 'react-router-dom'
+import { Spinner } from '../../../../components/Header/Spinner'
+import { relativeDateFormmater } from '../../../../utils/formatter'
+import { IPost } from '../../../Posts'
 import { PostInfoContainer } from './styles'
 
-export function PostInfo() {
+interface PostInfoProps {
+  post: IPost
+  isLoading: boolean
+}
+
+export function PostInfo({ post, isLoading }: PostInfoProps) {
   const navigate = useNavigate()
 
   function goBack() {
     navigate(-1)
   }
 
+  const formattedDate = relativeDateFormmater(post?.created_at)
+
   return (
     <PostInfoContainer>
-      <nav>
-        <button onClick={goBack}>
-          <FontAwesomeIcon icon={faChevronLeft} />
-          <span>VOLTAR</span>
-        </button>
-        <a href="#" target="_blank" rel="noreferrer">
-          <span>VER NO GITHUB</span>
-          <FontAwesomeIcon icon={faUpRightFromSquare} />
-        </a>
-      </nav>
-      <h2>Javascript data types and data structures</h2>
-      <ul>
-        <li>
-          <FontAwesomeIcon icon={faGithub} />
-          henriquesbarros
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faCalendar} />
-          Há 1 dia
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faComment} />
-          {'5 comentário(s)'}
-        </li>
-      </ul>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <nav>
+            <button onClick={goBack}>
+              <FontAwesomeIcon icon={faChevronLeft} />
+              <span>VOLTAR</span>
+            </button>
+            <a href={post.html_url} target="_blank" rel="noreferrer">
+              <span>VER NO GITHUB</span>
+              <FontAwesomeIcon icon={faUpRightFromSquare} />
+            </a>
+          </nav>
+          <h2>{post.title}</h2>
+          <ul>
+            <li>
+              <FontAwesomeIcon icon={faGithub} />
+              {post.user?.login}
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faCalendar} />
+              {formattedDate}
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faComment} />
+              {`${post.comments} comentário(s)`}
+            </li>
+          </ul>
+        </>
+      )}
     </PostInfoContainer>
   )
 }
--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,10 +1,42 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { api } from '../../lib/axios'
+import { IPost } from '../Posts'
+import { PostContent } from './components/PostContent'
 import { PostInfo } from './components/PostInfo'
 import { PostContainer } from './styles'
 
+const username = import.meta.env.VITE_GITHUB_USERNAME
+const reponame = import.meta.env.VITE_GITHUB_REPONAME
+
 export function Post() {
+  const [post, setPost] = useState({} as IPost)
+  const [isLoading, setIsLoading] = useState(true)
+
+  const { id } = useParams()
+
+  const getPost = async () => {
+    try {
+      setIsLoading(true)
+
+      const response = await api.get(
+        `/repos/${username}/${reponame}/issues/${id}`,
+      )
+
+      setPost(response.data)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getPost()
+  }, [])
+
   return (
     <PostContainer>
-      <PostInfo />
+      <PostInfo post={post} isLoading={isLoading} />
+      <PostContent />
     </PostContainer>
   )
 }
